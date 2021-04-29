5d086beda78eeeca0b598b16981d972338ebefd3
André Luis Sehnem <als10andr@hotmail.com>
2021-04-29 00:08:07 -0300

    alteração nome

--- FormatadorDePostagens/Properties/Resources.Designer.cs
+++ FormatadorDePostagens/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FormatadorDeVersoes.Properties {
+namespace FormatadorDePostagens.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace FormatadorDeVersoes.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FormatadorDeVersoes.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FormatadorDePostagens.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- FormatadorDePostagens/Properties/Settings.Designer.cs
+++ FormatadorDePostagens/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FormatadorDeVersoes.Properties {
+namespace FormatadorDePostagens.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
