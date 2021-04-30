853dffb0212e3d60657e2d2cc62ba42bd271ae9e
André Luis Sehnem <als10andr@hotmail.com>
2021-04-29 22:52:35 -0300

    Tela de login

--- FormatadorDePostagens/frm_menu.Designer.cs
+++ FormatadorDePostagens/frm_menu.Designer.cs
@@ -30,18 +30,145 @@ namespace FormatadorDePostagens
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_menu));
+            this.txt_nomeUsuario = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bt_conectar = new System.Windows.Forms.Button();
+            this.bt_criarBD = new System.Windows.Forms.Button();
+            this.lbl_servidor = new System.Windows.Forms.Label();
+            this.lbl_banco = new System.Windows.Forms.Label();
+            this.lbl_porta = new System.Windows.Forms.Label();
+            this.lbl_user = new System.Windows.Forms.Label();
+            this.lbl_pass = new System.Windows.Forms.Label();
+            this.txt_servidor = new System.Windows.Forms.TextBox();
+            this.txt_porta = new System.Windows.Forms.TextBox();
+            this.text_user = new System.Windows.Forms.TextBox();
+            this.txt_senha = new System.Windows.Forms.TextBox();
+            this.txt_banco = new System.Windows.Forms.TextBox();
+            this.bt_sair = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txt_nomeUsuario
+            // 
+            resources.ApplyResources(this.txt_nomeUsuario, "txt_nomeUsuario");
+            this.txt_nomeUsuario.Name = "txt_nomeUsuario";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // bt_conectar
+            // 
+            resources.ApplyResources(this.bt_conectar, "bt_conectar");
+            this.bt_conectar.Name = "bt_conectar";
+            this.bt_conectar.UseVisualStyleBackColor = true;
+            // 
+            // bt_criarBD
+            // 
+            resources.ApplyResources(this.bt_criarBD, "bt_criarBD");
+            this.bt_criarBD.Name = "bt_criarBD";
+            this.bt_criarBD.UseVisualStyleBackColor = true;
+            // 
+            // lbl_servidor
+            // 
+            resources.ApplyResources(this.lbl_servidor, "lbl_servidor");
+            this.lbl_servidor.Name = "lbl_servidor";
+            // 
+            // lbl_banco
+            // 
+            resources.ApplyResources(this.lbl_banco, "lbl_banco");
+            this.lbl_banco.Name = "lbl_banco";
+            // 
+            // lbl_porta
+            // 
+            resources.ApplyResources(this.lbl_porta, "lbl_porta");
+            this.lbl_porta.Name = "lbl_porta";
+            // 
+            // lbl_user
+            // 
+            resources.ApplyResources(this.lbl_user, "lbl_user");
+            this.lbl_user.Name = "lbl_user";
+            // 
+            // lbl_pass
+            // 
+            resources.ApplyResources(this.lbl_pass, "lbl_pass");
+            this.lbl_pass.Name = "lbl_pass";
+            // 
+            // txt_servidor
+            // 
+            resources.ApplyResources(this.txt_servidor, "txt_servidor");
+            this.txt_servidor.Name = "txt_servidor";
+            // 
+            // txt_porta
+            // 
+            resources.ApplyResources(this.txt_porta, "txt_porta");
+            this.txt_porta.Name = "txt_porta";
+            // 
+            // text_user
+            // 
+            resources.ApplyResources(this.text_user, "text_user");
+            this.text_user.Name = "text_user";
+            // 
+            // txt_senha
+            // 
+            resources.ApplyResources(this.txt_senha, "txt_senha");
+            this.txt_senha.Name = "txt_senha";
+            // 
+            // txt_banco
+            // 
+            resources.ApplyResources(this.txt_banco, "txt_banco");
+            this.txt_banco.Name = "txt_banco";
+            // 
+            // bt_sair
+            // 
+            resources.ApplyResources(this.bt_sair, "bt_sair");
+            this.bt_sair.Name = "bt_sair";
+            this.bt_sair.UseVisualStyleBackColor = true;
             // 
             // frm_menu
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.bt_sair);
+            this.Controls.Add(this.txt_banco);
+            this.Controls.Add(this.txt_senha);
+            this.Controls.Add(this.text_user);
+            this.Controls.Add(this.txt_porta);
+            this.Controls.Add(this.txt_servidor);
+            this.Controls.Add(this.lbl_pass);
+            this.Controls.Add(this.lbl_user);
+            this.Controls.Add(this.lbl_porta);
+            this.Controls.Add(this.lbl_banco);
+            this.Controls.Add(this.lbl_servidor);
+            this.Controls.Add(this.bt_criarBD);
+            this.Controls.Add(this.bt_conectar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_nomeUsuario);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
             this.Name = "frm_menu";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txt_nomeUsuario;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bt_conectar;
+        private System.Windows.Forms.Button bt_criarBD;
+        private System.Windows.Forms.Label lbl_servidor;
+        private System.Windows.Forms.Label lbl_banco;
+        private System.Windows.Forms.Label lbl_porta;
+        private System.Windows.Forms.Label lbl_user;
+        private System.Windows.Forms.Label lbl_pass;
+        private System.Windows.Forms.TextBox txt_servidor;
+        private System.Windows.Forms.TextBox txt_porta;
+        private System.Windows.Forms.TextBox text_user;
+        private System.Windows.Forms.TextBox txt_senha;
+        private System.Windows.Forms.TextBox txt_banco;
+        private System.Windows.Forms.Button bt_sair;
     }
 }
 
--- FormatadorDePostagens/frm_menu.cs
+++ FormatadorDePostagens/frm_menu.cs
@@ -7,14 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using Microsoft.SqlServer;
+using MySql.Data.MySqlClient;
 
 namespace FormatadorDePostagens
 {
     public partial class frm_menu : Form
     {
+        public MySqlConnection cnn = new MySqlConnection();
+
         public frm_menu()
         {
             InitializeComponent();
+            //lbl_statusMenu.Text = "Banco não conectado";
+            var nomeDoComputador = Environment.MachineName;
+            conectaBd();        
+            String comandoLogin = "insert into log_login (pc,appLanguage) VALUES('" + nomeDoComputador + "', 'C#')";
+            //comandoSql(comandoLogin);
         }
+
+        
+        public void conectaBd()
+        {
+            try
+            {
+                cnn.ConnectionString = "server=localhost;database=lojinha;uid=root;pwd=admin;SslMode=none";
+                cnn.Open();
+                //lbl_statusMenu.Text = "Conectado em " + cnn.Database;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        
     }
 }
